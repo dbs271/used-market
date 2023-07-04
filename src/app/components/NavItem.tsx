c54eff4c860242725569362f6e3645553512a0d3
윤국현 <djaudwls7873@gmail.com>
2023-07-05 00:10:05 +0900

    nav

---
+++ src/app/components/NavItem.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+const NavItem = ({ mobile }: { mobile?: boolean }) => {
+  return (
+    <ul
+      className={`text-md justify-center flex gap-4 w-full items-center ${
+        mobile && "flex-col h-full"
+      }`}
+    >
+      <li className="py-2 text-center border-b-4 cursor-pointer">
+        <Link href="/admin">Admin</Link>
+      </li>
+      <li className="py-2 text-center border-b-4 cursor-pointer">
+        <Link href="/user">User</Link>
+      </li>
+      <li className="py-2 text-center border-b-4 cursor-pointer">
+        <button>SignOut</button>
+      </li>
+      <li className="py-2 text-center border-b-4 cursor-pointer">
+        <button>SignIn</button>
+      </li>
+    </ul>
+  );
+};
+
+export default NavItem;

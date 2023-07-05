65d3b35391f18c1cdfebcb07c69ebc518a34ace7
윤국현 <djaudwls7873@gmail.com>
2023-07-05 14:43:02 +0900

    login 경로처리

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,6 +1,10 @@
+import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 
 const NavItem = ({ mobile }: { mobile?: boolean }) => {
+  const { data: session, status } = useSession();
+  console.log({ session }, status);
+
   return (
     <ul
       className={`text-md justify-center flex gap-4 w-full items-center ${
@@ -13,12 +17,15 @@ const NavItem = ({ mobile }: { mobile?: boolean }) => {
       <li className="py-2 text-center border-b-4 cursor-pointer">
         <Link href="/user">User</Link>
       </li>
-      <li className="py-2 text-center border-b-4 cursor-pointer">
-        <button>SignOut</button>
-      </li>
-      <li className="py-2 text-center border-b-4 cursor-pointer">
-        <button>SignIn</button>
-      </li>
+      {session?.user ? (
+        <li className="py-2 text-center border-b-4 cursor-pointer">
+          <button onClick={() => signOut()}>SignOut</button>
+        </li>
+      ) : (
+        <li className="py-2 text-center border-b-4 cursor-pointer">
+          <button onClick={() => signIn()}>SignIn</button>
+        </li>
+      )}
     </ul>
   );
 };

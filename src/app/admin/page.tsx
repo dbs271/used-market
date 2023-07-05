c1a070b39cfd809e190a3edb54ea2d5868ce4be8
윤국현 <djaudwls7873@gmail.com>
2023-07-05 11:53:09 +0900

    prisma, docker

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,5 @@
+const AdminPage = () => {
+  return <div>관리자만 볼 수 있는 페이지</div>;
+};
+
+export default AdminPage;
---
+++ src/app/user/page.tsx
@@ -0,0 +1,5 @@
+const UserPage = () => {
+  return <div>로그인된 유저만 볼 수 있는 페이지</div>;
+};
+
+export default UserPage;

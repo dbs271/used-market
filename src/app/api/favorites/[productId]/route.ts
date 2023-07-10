85573e818b41d2367a964411718a0ea2f145e711
윤국현 <djaudwls7873@gmail.com>
2023-07-10 19:43:20 +0900

    favorite post route 생성

---
+++ src/app/api/favorites/[productId]/route.ts
@@ -0,0 +1,32 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+
+interface Params {
+  productId?: string;
+}
+
+export async function POST(req: Request, { params }: { params: Params }) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { productId } = params;
+
+  if (!productId || typeof productId !== "string") {
+    throw new Error(`Invalid ID`);
+  }
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds.push(productId);
+
+  const user = await prisma?.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds: favoriteIds,
+    },
+  });
+  return NextResponse.json(user);
+}

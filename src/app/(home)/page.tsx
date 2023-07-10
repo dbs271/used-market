bd864db6db17514c6ec714e8b7a60ba4a311147c
윤국현 <djaudwls7873@gmail.com>
2023-07-10 16:40:31 +0900

    홈페이지 데이터 생성

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import getProducts, { ProductsParams } from "../actions/getProducts";
+
+interface HomeProps {
+  searchParams: ProductsParams;
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const products = await getProducts(searchParams);
+
+  return <main>누구나 볼 수 있는 페이지</main>;
+}
---
+++ src/app/actions/getProducts.ts
@@ -0,0 +1,36 @@
+import prisma from "../../helpers/prismadb";
+
+export interface ProductsParams {
+  latitude?: number;
+  longitude?: number;
+  category?: string;
+}
+
+export default async function getProducts(params: ProductsParams) {
+  try {
+    const { latitude, longitude, category } = params;
+    let query: any = {};
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (latitude) {
+      query.latitude = {
+        gte: Number(latitude) - 0.01,
+        lte: Number(longitude) + 0.01,
+      };
+    }
+    const products = await prisma.product.findMany({
+      where: query,
+      orderBy: {
+        createAt: "desc",
+      },
+    });
+    return {
+      data: products,
+    };
+  } catch (err: any) {
+    throw new Error(err);
+  }
+}

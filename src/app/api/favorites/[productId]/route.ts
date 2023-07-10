176393abfc901b958442eccf266921765ca25033
윤국현 <djaudwls7873@gmail.com>
2023-07-10 19:46:48 +0900

    favorite delete route 생성

--- src/app/api/favorites/[productId]/route.ts
+++ src/app/api/favorites/[productId]/route.ts
@@ -30,3 +30,29 @@ export async function POST(req: Request, { params }: { params: Params }) {
   });
   return NextResponse.json(user);
 }
+
+export async function DELETE(req: Request, { params }: { params: Params }) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { productId } = params;
+
+  if (!productId || typeof productId !== "string") {
+    throw new Error(`Invalid ID`);
+  }
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds = favoriteIds.filter((id) => id !== productId);
+
+  const user = await prisma?.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds: favoriteIds,
+    },
+  });
+  return NextResponse.json(user);
+}

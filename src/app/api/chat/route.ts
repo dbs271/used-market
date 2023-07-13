ab3b8eb3b59cb42161f62480bbb484448ee40ee7
윤국현 <djaudwls7873@gmail.com>
2023-07-14 00:28:27 +0900

    채팅 메시지 route 생성

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -27,3 +27,83 @@ export async function GET(req: Request) {
   });
   return NextResponse.json(users);
 }
+
+export async function POST(req: Request) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+  const body = await req.json();
+
+  // 이미 둘이 대화를 한 conversation 이 있는지 찾기
+
+  const conversation = await prisma.conversation.findFirst({
+    where: {
+      AND: [
+        {
+          users: {
+            some: {
+              id: body.senderId,
+            },
+          },
+        },
+        {
+          users: {
+            some: {
+              id: body.receiverId,
+            },
+          },
+        },
+      ],
+    },
+  });
+  if (conversation) {
+    // 이미 둘이 대화를 한 conversation 이 있다면 메시지만 생성
+    try {
+      const message = await prisma.message.create({
+        data: {
+          text: body.text,
+          image: body.image,
+          senderId: body.senderId,
+          receiverId: body.receiverId,
+          conversationId: conversation.id,
+        },
+      });
+      return NextResponse.json(message);
+    } catch (error) {
+      return NextResponse.json(error);
+    }
+  } else {
+    // 둘이 처음 대화하는 거라면 conversation 과 메시지 둘 다 생성
+    const newConversation = await prisma.conversation.create({
+      data: {
+        senderId: body.senderId,
+        receiverId: body.receiverId,
+        users: {
+          connect: [
+            {
+              id: body.senderId,
+            },
+            {
+              id: body.receiverId,
+            },
+          ],
+        },
+      },
+    });
+    try {
+      const message = await prisma.message.create({
+        data: {
+          text: body.text,
+          image: body.image,
+          senderId: body.senderId,
+          receiverId: body.receiverId,
+          conversationId: newConversation.id,
+        },
+      });
+      return NextResponse.json(message);
+    } catch (error) {
+      return NextResponse.json(error);
+    }
+  }
+}
